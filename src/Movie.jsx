f5c020a023d2de19ced1a5d030b440aac2123856
lawrencemasilo <66621783+lawrencemasilo@users.noreply.github.com>
2023-11-27 13:58:35 +0200

    Update Movie.jsx

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -6,7 +6,7 @@ import React from 'react'
     "Title": "James Bond 007: Everything or Nothing",
     "Year": "2003",
     "imdbID": "tt0366629",
-    "Type": "game",https://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@github.com/lawrencemasilo/Simple_Movie_Browser.git
+    "Type": "game,
     "Poster": "https://m.media-amazon.com/images/M/MV5BNzhiNjgxNGMtMzg0ZS00ODMwLTk5MTYtYzI0NDBmOThlZjY3XkEyXkFqcGdeQXVyNTEwNDY2MjU@._V1_SX300.jpg"
 }
 */
